9715236979bc7cd944ba27afa6dd06c4b0b15da1
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-03-28 15:55:28 +0800

    update

--- src/modules/routes/index.js
+++ src/modules/routes/index.js
@@ -1,11 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Content from '../content/index.js'
-import About from '../about/index.js'
+// import About from '../about/index.js'
 function RoutesList(){
     return (
         <Routes>
             <Route path="/" element={<Content/>}></Route>
-            <Route path="/about" element={<About/>}></Route>
+            {/* <Route path="/about" element={<About/>}></Route> */}
         </Routes>
     )
 }
