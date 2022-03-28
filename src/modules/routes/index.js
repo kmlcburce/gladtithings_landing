ba71d5e21c18b8b8db0336b882b017bd5ba98e4b
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-03-28 16:41:52 +0800

    update

--- src/modules/routes/index.js
+++ src/modules/routes/index.js
@@ -1,11 +1,13 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Content from '../content/index.js'
+import Content from '../content/about/index.js'
 function RoutesList(){
     return (
         <Routes>
             <Route path="/" element={<Content/>}></Route>
+            <Route path="/about" element={<Content/>}></Route>
         </Routes>
     )
 }
 
-export default RoutesList;
+export default RoutesList;y
