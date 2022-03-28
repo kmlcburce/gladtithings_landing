d4fc8850825b0b4abdb63e196d8c35c96f145f43
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-03-28 16:50:07 +0800

    routes fix

--- src/modules/routes/index.js
+++ src/modules/routes/index.js
@@ -1,19 +1,13 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Content from '../content/index.js'
-<<<<<<< HEAD
-import Content from '../content/about/index.js'
-=======
-// import About from '../about/index.js'
->>>>>>> 18dde6f54ec43fcf5709bb26b28577d2be28383c
+import About from '../content/about/index.js'
+import Contact from '../content/contact/index.js'
 function RoutesList(){
     return (
         <Routes>
             <Route path="/" element={<Content/>}></Route>
-<<<<<<< HEAD
-            <Route path="/about" element={<Content/>}></Route>
-=======
-            {/* <Route path="/about" element={<About/>}></Route> */}
->>>>>>> 18dde6f54ec43fcf5709bb26b28577d2be28383c
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/contact" element={<Contact/>}></Route>
         </Routes>
     )
 }
