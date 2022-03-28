2158ef0c52e4266b78aa9eff72457b803239158a
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-03-28 17:27:10 +0800

    updates

--- src/modules/content/donateModal.js
+++ src/modules/content/donateModal.js
@@ -11,39 +11,39 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
+	textAlign: 'center',
+	borderRadius: '2%',
   p: 4,
 };
 
-const DonateModal = ({modalRef}) => {
-  const [open, setOpen] = React.useState(false);
-	React.useEffect(() => {
-		
-	}, [])
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  return (
-    <div>
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
-  );
+class DonateModal extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			open: false
+		}
+	}
+  handleOpen = () => this.setState({open: true});
+  handleClose = () => this.setState({open: false});
+  
+	render(){
+		return (
+			<div>
+				<Modal
+					open={this.state.open}
+					onClose={() => this.handleClose()}
+					aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description"
+				>
+					<Box sx={style}>
+						<h3>Download the App to Donate!</h3>
+						<i><p>Donate to churches by using Gladtithings mobile application</p></i><br/>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu</p>
+					</Box>
+				</Modal>
+			</div>
+		);
+	}
 }
 
 export default DonateModal;
--- src/modules/header/index.js
+++ src/modules/header/index.js
@@ -7,32 +7,34 @@ import Button from '@mui/material/Button';
 import '../common/Style.css'
 import DonateModal from '../content/donateModal';
 
-const Header = () =>{
-  const modalRef = React.useRef();
-  const handleClick = () => {
-      console.log(modalRef);
+class Header extends React.Component{
+	constructor(props){
+		super(props);
+		this.modalRef = React.createRef()
+		this.state={}
+	}
+  handleClick = () => {
+    this.modalRef.current.handleOpen()
   }
-  React.useEffect(() => {
-      console.log('=====', modalRef);
-    // modalRef.current = 
-  }, [])
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" className='header' elevation={0}>
-        <Toolbar>
-          <img src={require('../assets/logo.png')} className="navImg"></img>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          </Typography>
-          <section>
-            <Button className='nav'>About</Button>
-            <Button className='nav'>Contact</Button>
-            <Button className='roundedBtn' color='inherit' onClick={handleClick}>Donate</Button>
-          </section>
-        </Toolbar>
-      </AppBar>
-      <DonateModal ref={modalRef}/>
-    </Box>
-  );
+	render(){
+		return (
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static" className='header' elevation={0}>
+					<Toolbar>
+						<img src={require('../assets/logo.png')} className="navImg"></img>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						</Typography>
+						<section>
+							<Button className='nav'>About</Button>
+							<Button className='nav'>Contact</Button>
+							<Button className='roundedBtn' color='inherit' onClick={() => this.handleClick()}>Donate</Button>
+						</section>
+					</Toolbar>
+				</AppBar>
+				<DonateModal ref={this.modalRef}/>
+			</Box>
+		);
+	}
 }
 
 export default Header
