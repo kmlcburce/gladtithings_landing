a841b66c926e3c01725d3a207be9491997bd0ad8
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-03-30 16:29:36 +0800

    contact, routes

--- src/modules/routes/index.js
+++ src/modules/routes/index.js
@@ -1,13 +1,13 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Content from '../content/index.js'
 import About from '../content/about/index.js'
-// import Contact from '../content/contact/index.js'
+import Contact from '../content/contact/index.js'
 function RoutesList(){
     return (
         <Routes>
             <Route path="/" element={<Content/>}></Route>
             <Route path="/about" element={<About/>}></Route>
-            {/* <Route path="/contact" element={<Contact/>}></Route> */}
+            <Route path="/contact" element={<Contact/>}></Route>
         </Routes>
     )
 }
