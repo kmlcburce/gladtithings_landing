585b1675128537ddc43032f6ac273e401aed1ea4
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-04-01 21:54:52 +0800

    updates

--- src/modules/index.js
+++ src/modules/index.js
@@ -6,10 +6,28 @@ class Content extends Component {
 
   renderLeft(){
 		return (
-			<div>
-				<img src={require('assets/LogoLeft.png')} className="image"></img>
-				<div style={{marginTop: '22%'}}>
-					<img src={require('assets/saints.png')} className="image"></img>
+			<div style={{
+				height: '100vh',
+				display: 'contents',
+				justifyContent: 'space-between'
+			}}>
+				<div style={{
+					width: '100%',
+					float: 'left'
+				}}>
+				<img src={require('assets/LogoLeft.png')}   style={{
+						width: '100%',
+						height: 'auto'
+					}}></img>
+				</div>
+				<div style={{
+					width: '100%',
+					float: 'left'
+				}}>
+					<img src={require('assets/saints.png')}  style={{
+						width: '100%',
+						height: 'auto'
+					}}></img>
 				</div>
 			</div>
 		)
@@ -17,27 +35,19 @@ class Content extends Component {
 	renderRight(){
 		return(
 			<div>
-				<img src={require('assets/clouds.png')} className="image"></img>
+				<img src={require('assets/clouds.png')} className="image clouds"></img>
 			</div>
 		)
 	}
   render() {
     return (
       <div>
-          <section className='contentContainer' style={{justifyContent: 'space-between'}}>
-							<div className='leftContent'>
-								{this.renderLeft()}
-							</div>
-							<div className='rightContent'
-								style={{
-									backgroundImage: `url(${bgRight})`,
-									backgroundSize: 'cover',
-									backgroundRepeat: 'no-repeat',
-								}}
-							>
-								{this.renderRight()}
-							</div>
-					</section>
+        <section className='contentContainer'>
+			<img src={require('assets/landing.png')} style={{
+				width: '100%',
+				height: '100vh'
+			}}></img>		
+		</section>
       </div>
     )
   }
